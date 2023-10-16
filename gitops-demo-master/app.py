5e0793196f8cf8a110c1034b4b0726dfdeb7040b
ahmadchaabane26 <133824462+ahmadchaabane26@users.noreply.github.com>
2023-10-16 15:23:06 -0400

    Update app.py

--- gitops-demo-master/app.py
+++ gitops-demo-master/app.py
@@ -11,5 +11,4 @@ redis = FlaskRedis(app)
 
 @app.route('/')
 def counter():
-    return '{0} {1}'.format(str(redis.incr('web2_counter')),
-                            os.getenv('WEB2_COUNTER_MSG', ''))
+    return 'This is a test for docker !!!'
