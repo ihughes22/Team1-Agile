f47b990716c39f59d2e8f694686c8444b3b5846b
ihughes22 <75337283+ihughes22@users.noreply.github.com>
2023-10-16 13:40:31 -0400

    Adding Registration and Login Pages for React App

---
+++ digital-memory-book/src/Registration.js
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import './Registration.css';
+
+function Registration() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [isRegistered, setIsRegistered] = useState(false);
+  const [registrationError, setRegistrationError] = useState("");
+
+  const handleRegister = () => {
+    // Check if the email is valid.
+    if (!isValidEmail(email)) {
+      setRegistrationError("Invalid email address.");
+      return;
+    }
+
+	  // Check if the username is empty.
+	  if (!username) {
+		setRegistrationError("Please enter a username.");
+		return;
+	  }
+
+    // Check if the password and password confirmation match.
+    if (password !== passwordConfirmation) {
+      setRegistrationError("Passwords do not match.");
+      return;
+    }
+
+      // Check the password requirements.
+  const passwordValidationResult = validatePassword(password);
+  if (passwordValidationResult !== "valid") {
+    setRegistrationError(passwordValidationResult);
+    return;
+  }
+
+  // Here you would typically send the registration data to your backend for processing.
+  // For the sake of this example, we'll simulate success after a delay.
+  setTimeout(() => {
+    setIsRegistered(true);
+    setUsername(username);
+  }, 1000);
+};
+
+const validatePassword = (password) => {
+	const letterCount = password.replace(/[^a-zA-Z]/g, "").length;
+	if (letterCount < 8) {
+	  return "Password must contain at least 8 letters.";
+	}
+	if (!/\d/.test(password)) {
+	  return "Password must contain at least one number.";
+	}
+	if (!/[!@#$%^&*]/.test(password)) {
+	  return "Password must contain a special character (!@#$%^&*).";
+	}
+	return "valid";
+  };  
+
+  const isValidEmail = (email) => {
+    // You can implement your own email validation logic here.
+    // For a basic check, you can use regular expressions.
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return emailRegex.test(email);
+  };
+
+  const handleLogout = () => {
+    setIsRegistered(false);
+    setEmail("");
+    setUsername("");
+    setPassword("");
+    setPasswordConfirmation("");
+    setRegistrationError("");
+  };
+
+  return (
+    <div className="RegistrationPage">
+      {isRegistered ? (
+        <div>
+          <h2>Welcome, {username}!</h2>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <h2>Register</h2>
+          <p>{registrationError}</p>
+          <div>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              value={passwordConfirmation}
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
+            />
+          </div>
+          <button onClick={handleRegister}>Register</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Registration;
