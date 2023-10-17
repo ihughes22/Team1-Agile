6841d4354a0004b902de38ebf915249fd10299b4
kethan351 <kethan351@gmail.com>
2023-10-17 18:03:31 -0400

    auth

--- Backend/src/components/auth.js
+++ Backend/src/components/auth.js
@@ -1,27 +1,45 @@
 import { auth } from "../config/firebase";
-import {createUserWithEmailAndPassword} from "firebase/auth"
+import {
+    createUserWithEmailAndPassword,
+    signOut,
+    } from "firebase/auth";
+import { useState } from "react";
 
 export const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     const signIn = async () => {
-        await createUserWithEmailAndPassword(auth, email, password);
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+          } catch (err) {
+            console.error(err);
+          }
     }; //I am currently watching https://www.youtube.com/watch?v=2hR-uWjBAgw&ab_channel=PedroTech
     //I am at 19:57
-    return (
+    
+      const logout = async () => {
+        try {
+          await signOut(auth);
+        } catch (err) {
+          console.error(err);
+        }
+      };
+    
+      return (
         <div>
-            <input
-                placeholder = "Email..."
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-                placeholder = "Password..."
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={signIn}> Sign In</button>
+          <input
+            placeholder="Email..."
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="Password..."
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={signIn}> Sign In</button>
+    
+          <button onClick={logout}> Logout </button>
         </div>
-    );
-
-
-};
+      );
+    };
