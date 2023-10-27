80cf4902bb8793eb7223f544b8c3dde67ce7139a
Ashna Razdan <ashnarazdan123@gmail.com>
2023-10-27 15:21:41 -0400

    created the popup for the modal

--- digital-memory-book/src/DigitalTimeline.js
+++ digital-memory-book/src/DigitalTimeline.js
@@ -1,13 +1,40 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { Component } from "react";
+import "./DigitalTimeline.css";
 
-function DigitalTimeline() {
-  function newTimeline() {}
-  return (
-    <div>
-      <Button onClick={newTimeline}>New Timeline</Button>
-    </div>
-  );
+class DigitalTimeline extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPopupOpen: false,
+    };
+  }
+
+  openPopup = () => {
+    this.setState({ isPopupOpen: true });
+  };
+
+  closePopup = () => {
+    this.setState({ isPopupOpen: false });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.openPopup}>Create New Timeline</button>
+        {this.state.isPopupOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={this.closePopup}>
+                &times;
+              </span>
+              <p>Hi</p>
+              <button onClick={this.closePopup}>Close</button>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default DigitalTimeline;
