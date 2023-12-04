72a08a58729587b6f1d825100c8f82edbe42613b
Jolene Ciccarone <jciccaro@stevens.edu>
2023-12-03 22:56:48 -0500

    finally fetched some posts

--- digital-memory-book/src/NewPhotobook.js
+++ digital-memory-book/src/NewPhotobook.js
@@ -1,101 +1,64 @@
-import React, { useState, useEffect } from "react";
 import HTMLFlipBook from "react-pageflip";
-import {
-  collection,
-  getDocs,
-} from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
 import { db } from "./firebase";
-import { useNavigate } from "react-router-dom";
-import "./BookExample.css"; // Import the CSS file
-import backgroundImage from './Photos/triangle-mosaic.png';
+import "./BookExample.css";
 
-const NewBookExample = ({ isAuth }) => {
-  const pageStyles = {
-    textAlign: 'center',
-    padding: '20px',
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: '500px 500px',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'repeat',
-    height: '100vh',
-  };
-
-  const PageCover = React.forwardRef((props, ref) => (
-    <div className="cover" ref={ref} data-density="hard">
-      <div>
-        <h2>{props.children}</h2>
-      </div>
+const PageCover = React.forwardRef((props, ref) => (
+  <div className="cover" ref={ref} data-density="hard">
+    <div>
+      <h2>{props.children}</h2>
     </div>
-  ));
+  </div>
+));
 
-  const Page = React.forwardRef(({ post, number }, ref) => (
+const Page = React.forwardRef(({ fetchData }, ref) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchAllPosts = async () => {
+      try {
+        const postsRef = collection(db, "posts");
+        const postsData = await getDocs(postsRef);
+        const allPosts = [];
+
+        postsData.forEach((doc) => {
+          const ddata = doc.data();
+          allPosts.push({ id: doc.id, ...ddata });
+        });
+
+        allPosts.sort((a, b) => new Date(b.date) - new Date(a.date));
+        setPosts(allPosts);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    fetchAllPosts();
+  }, []);
+
+  return (
     <div className="page" ref={ref}>
       <h1>Page Header</h1>
-      <p>{number}</p>
-      <div id="post-container" onLoad={fetchPosts}>
-        <h1 className="post-heading">Photobook</h1>
-        {post ? (
-          <div key={post.id} className="post-item">
-            <img className="post-image" src={post.path} alt="Post" />
-            <div className="post-details">
-              <p className="post-caption">{post.caption}</p>
-              <p className="post-date">{post.date}</p>
-            </div>
+      {posts.map((post) => (
+        <div key={post.id} className="post-item">
+          <img className="post-image" src={post.path} alt="Post" />
+          <div className="post-details">
+            <p className="post-caption">{post.caption}</p>
+            <p className="post-date">{post.date}</p>
           </div>
-        ) : (
-          <p>No post available for this page</p>
-        )}
-      </div>
+        </div>
+      ))}
     </div>
-  ));
-  
-  
+  );
+});
 
-  const [posts, setPosts] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [code, setCode] = useState(localStorage.getItem("code") || "");
-  const [uid, setUid] = useState(localStorage.getItem("uid") || "");
-  const postsPerPage = 1;
+function BookExample(props) {
+  // Set the desired number of content pages
+  const numberOfPages = 10;
 
-  const fetchPosts = async () => {
-    try {
-      const postsRef = collection(db, "posts");
-      const postsData = await getDocs(postsRef);
-      const matchingUsers = postsData
-        .filter(doc => doc.data().code === code)
-        .map(doc => ({ id: doc.id, ...doc.data() }));
-
-      matchingUsers.sort((a, b) => new Date(b.date) - new Date(a.date));
-      setPosts(matchingUsers);
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchPosts();
-    const intervalId = setInterval(fetchPosts, 30 * 1000 * 60);
-    return () => clearInterval(intervalId);
-  }, [code]);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!isAuth) {
-      navigate("/login");
-    }
-  }, [isAuth, navigate]);
-
-  const [inputText, setInputElement] = useState("");
-  const [text, setText] = useState("sample text");
-
-  const printText = () => {
-    setText(inputText);
-    setInputElement("");
-  };
-
-  const TotalPages = 10;
-  const contentPageNumbers = Array.from({ length: TotalPages }, (_, index) => index + 1);
+  // Create an array of page numbers for content pages
+  const contentPageNumbers = Array.from({ length: numberOfPages }, (_, index) => index + 1);
 
   return (
     <div>
@@ -112,16 +75,19 @@ const NewBookExample = ({ isAuth }) => {
         maxShadowOpacity={0.5}
         className="album-web"
       >
+        {/* Front Cover */}
         <PageCover>Front Cover</PageCover>
 
-        {contentPageNumbers.map((pageNumber, index) => (
-        <Page key={pageNumber} post={posts[index]} number={pageNumber} />
+        {/* Content Pages */}
+        {contentPageNumbers.map((pageNumber) => (
+          <Page key={pageNumber} />
         ))}
 
+        {/* Back Cover */}
         <PageCover>Back Cover</PageCover>
       </HTMLFlipBook>
     </div>
   );
-};
+}
 
-export default NewBookExample;
+export default BookExample;
