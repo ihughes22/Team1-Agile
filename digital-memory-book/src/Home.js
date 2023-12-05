68ac671418741e6063b925632267ef301007a2f9
Ashna Razdan <ashnarazdan123@gmail.com>
2023-12-05 14:33:23 -0500

    refactoring home and css

--- digital-memory-book/src/Home.js
+++ digital-memory-book/src/Home.js
@@ -1,49 +1,32 @@
-import React from 'react';
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./Interactable.css"
-import backgroundImage from './Photos/triangle-mosaic.png'
+import "./Interactable.css";
+import "./Home.css";
+import backgroundImage from "./Photos/triangle-mosaic.png";
 
-const Home = ({ isAuth}) => {
-  const pageStyles = {
-    textAlign: 'center',
-    padding: '20px',
-    backgroundImage: `url(${backgroundImage})`,  // Add this line
-    backgroundSize: '500px 500px',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'repeat',
-    height: '100vh',
-  };
-
-  const headingStyles = {
-    fontSize: '36px',
-    fontWeight: 'bold',
-    color: '#333',
-    marginBottom: '20px',
-  };
-
-  const subheadingStyles = {
-    fontSize: '18px',
-    color: '#666',
-    marginBottom: '30px',
-  };
-
-
+const Home = ({ isAuth }) => {
   const navigate = useNavigate();
 
   const handleClick2 = () => {
-    navigate('/login')
+    navigate("/login");
   };
 
   return (
-    <div style={pageStyles}>
+    <div
+      className="page"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
       <div>
-        <h1 style={headingStyles}>Welcome to My Memory Book Software</h1>
-        <p style={subheadingStyles}>Capture and relive your memories with ease.</p>
+        <h1 className="heading">Welcome to My Memory Book Software</h1>
+        <p className="subheading">
+          Capture and relive your memories with ease.
+        </p>
         {!isAuth ? (
-          <button onClick = {handleClick2} className="login-button">Log In</button>
-        ) :
-        (
-          <p style={subheadingStyles}>Glad you're here!</p>
+          <button onClick={handleClick2} className="login-button">
+            Log In
+          </button>
+        ) : (
+          <p className="subheading">Glad you're here!</p>
         )}
       </div>
     </div>
